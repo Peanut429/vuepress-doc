ec071dbda78e8ea615f31d33937ebbccbdb83ef7
Peanut <18915000292@163.com>
2020-11-30 22:54:59 +0800

    feat: add reco theme

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  theme: 'reco',
+  title: 'hello Vuepress',
+  description: 'my blog',
+  head: [
+    ['meta', {
+      name: 'viewport',
+      content: 'width=device-width,initial-scale=1,user-scalable=no'
+    }],
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  themeConfig: {
+    type: 'blog',
+    author: 'Peanut',
+    authorAvatar: '/logo.png',
+    subSidebar: 'auto',
+    blogConfig: {
+      category: {
+        location: 2,     // 在导航栏菜单中所占的位置，默认2
+        text: '分类' // 默认文案 “分类”
+      },
+      tag: {
+        location: 3,     // 在导航栏菜单中所占的位置，默认3
+        text: '标签'      // 默认文案 “标签”
+      }
+    },
+    friendLink: [
+      // {
+      //   title: 'vuepress-theme-reco',
+      //   desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+      //   logo: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+      //   link: 'https://vuepress-theme-reco.recoluan.com'
+      // },
+      // {
+      //   title: '午后南杂',
+      //   desc: 'Enjoy when you can, and endure when you must.',
+      //   email: 'recoluan@qq.com',
+      //   link: 'https://www.recoluan.com'
+      // }
+    ]
+  }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,10 @@
-# hello vuepress
+---
+home: true
+heroText: null
+tagline: null
+bgImage: '/home-bg.jpg'
+bgImageStyle:
+  height: '50vw'
+  objectFill: 'cover'
+  backgroundPosition: 'left bottom'
+---
